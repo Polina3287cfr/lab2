c2033e95ade0634a496608ece94956254cecf2a9
Polina3287cfr <pletos.polina.p2114@gmail.com>
2024-01-23 09:24:32 +0200

    init

---
+++ polina2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polina2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 polina = new Form3();
+            polina.Show();
+        }
+    }
+}
